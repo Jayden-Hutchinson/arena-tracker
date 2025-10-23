9dcc0e1761e75b4517fb34577037733ceaf47642
Jayden H <jaydenhutchinson747@gmail.com>
2025-10-23 14:34:43 -0700

    push

--- server/src/api/riot/RiotApiRoutes.js
+++ server/src/api/riot/RiotApiRoutes.js
@@ -4,36 +4,26 @@ const BASE_URL = {
 };
 
 const RIOT_API_ROUTE = {
-  ACCOUNT: {
-    BY_GAME_NAME: (gameName, tagLine) =>
-      `${BASE_URL.REGIONAL}/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`,
-    BY_PUUID: (puuid) =>
-      `${BASE_URL.REGIONAL}/riot/account/v1/accounts/by-puuid/${puuid}`,
-  },
+  ACCOUNT_BY_GAME_NAME: (gameName, tagLine) =>
+    `${BASE_URL.REGIONAL}/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`,
+  ACCOUNT_BY_PUUID: (puuid) =>
+    `${BASE_URL.REGIONAL}/riot/account/v1/accounts/by-puuid/${puuid}`,
 
-  SUMMONER: {
-    BY_PUUID: (puuid) =>
-      `${BASE_URL.PLATFORM}/lol/summoner/v4/summoners/by-puuid/${puuid}`,
-  },
-  MATCH: {
-    BY_PUUID: (
-      puuid,
-      start = null,
-      count = null,
-      queue = null,
-      startTime = null
-    ) => {
-      const url = new URL(
-        `${BASE_URL.REGIONAL}/lol/match/v5/matches/by-puuid/${puuid}/ids?`
-      );
-      if (start != null) url.searchParams.append("start", start);
-      if (count != null) url.searchParams.append("count", count);
-      if (queue != null) url.searchParams.append("queue", queue);
-      if (startTime != null) url.searchParams.append("startTime", startTime);
-      return url.href;
-    },
-    BY_ID: (matchId) => `${BASE_URL.REGIONAL}/lol/match/v5/matches/${matchId}`,
+  SUMMONER_BY_PUUID: (puuid) =>
+    `${BASE_URL.PLATFORM}/lol/summoner/v4/summoners/by-puuid/${puuid}`,
+
+  MATCH_BY_ID: (matchId) =>
+    `${BASE_URL.REGIONAL}/lol/match/v5/matches/${matchId}`,
+
+  MATCH_BY_PUUID: (puuid, start, count, queue, startTime) => {
+    const url = new URL(
+      `${BASE_URL.REGIONAL}/lol/match/v5/matches/by-puuid/${puuid}/ids?`
+    );
+    if (start) url.searchParams.append("start", start);
+    if (count) url.searchParams.append("count", count);
+    if (queue) url.searchParams.append("queue", queue);
+    if (startTime) url.searchParams.append("startTime", startTime);
+    return url.href;
   },
 };
-
 module.exports = { RIOT_API_ROUTE };
