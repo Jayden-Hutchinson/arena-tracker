e2c5ecd1944db3e4d735016cf524bb187c532677
Jayden H <jaydenhutchinson747@gmail.com>
2025-10-23 14:25:27 -0700

    push

--- server/src/api/riot/RiotApiRoutes.js
+++ server/src/api/riot/RiotApiRoutes.js
@@ -3,26 +3,17 @@ const BASE_URL = {
   REGIONAL: `https://americas.api.riotgames.com`,
 };
 
-const ENDPOINT = {
-  ACCOUNTS: `${BASE_URL.REGIONAL}/riot/account/v1/accounts`,
-  SUMMONERS: `${BASE_URL.PLATFORM}/lol/summoner/v4/summoners`,
-  MATCHES: `${BASE_URL.REGIONAL}/lol/match/v5/matches`,
-};
-
-const METHOD = {
-  BY_ID: "by-riot-id",
-  BY_PUUID: "by-puuid",
-};
-
 const RIOT_API_ROUTE = {
   ACCOUNT: {
     BY_GAME_NAME: (gameName, tagLine) =>
-      `${ENDPOINT.ACCOUNTS}/${METHOD.BY_ID}/${gameName}/${tagLine}`,
-    BY_PUUID: (puuid) => `${ENDPOINT.ACCOUNTS}/${METHOD.BY_PUUID}/${puuid}`,
+      `${BASE_URL.REGIONAL}/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`,
+    BY_PUUID: (puuid) =>
+      `${BASE_URL.REGIONAL}/riot/account/v1/accounts/by-puuid/${puuid}`,
   },
 
   SUMMONER: {
-    BY_PUUID: (puuid) => `${ENDPOINT.SUMMONERS}/${METHOD.BY_PUUID}/${puuid}`,
+    BY_PUUID: (puuid) =>
+      `${BASE_URL.PLATFORM}/lol/summoner/v4/summoners/by-puuid/${puuid}`,
   },
   MATCH: {
     BY_PUUID: (
@@ -30,16 +21,18 @@ const RIOT_API_ROUTE = {
       start = null,
       count = null,
       queue = null,
-      startTime = null,
+      startTime = null
     ) => {
-      const url = new URL(`${ENDPOINT.RIOT.MATCHES}/by-puuid/${puuid}/ids?`);
+      const url = new URL(
+        `${BASE_URL.REGIONAL}/lol/match/v5/matches/by-puuid/${puuid}/ids?`
+      );
       if (start != null) url.searchParams.append("start", start);
       if (count != null) url.searchParams.append("count", count);
       if (queue != null) url.searchParams.append("queue", queue);
       if (startTime != null) url.searchParams.append("startTime", startTime);
       return url.href;
     },
-    BY_ID: (matchId) => `${ENDPOINT.RIOT.MATCHES}/${matchId}`,
+    BY_ID: (matchId) => `${BASE_URL.REGIONAL}/lol/match/v5/matches/${matchId}`,
   },
 };
 
