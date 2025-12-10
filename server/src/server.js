4ef9b58fc7403579d0751b1e78e9f4556bc6d40c
Jayden <jaydenhutchinson747@gmail.com>
2025-12-10 14:02:37 -0800

    deleted logs

--- server/src/server.js
+++ server/src/server.js
@@ -14,20 +14,15 @@ const RIOT_API_KEY = process.env.RIOT_API_KEY;
  SERVER URLS
 */
 const SERVER_BASE_URL = "/arena-tracker/api";
-
 const URL = {
   /* ACCOUNT */
   ACCOUNT_BY_GAME_NAME: `${SERVER_BASE_URL}/riot/account/by-riot-id/`,
-  ACCOUNT_BY_PUUID : `${SERVER_BASE_URL}/riot/account/by-puuid/`,
+  ACCOUNT_BY_PUUID: `${SERVER_BASE_URL}/riot/account/by-puuid/`,
   /* MATCHES */
-  MATCHES_BY_PUUID : `${SERVER_BASE_URL}/riot/account/matches/by-puuid`,
-  MATCH_BY_ID : `${SERVER_BASE_URL}/riot/match/`
-}
+  MATCHES_BY_PUUID: `${SERVER_BASE_URL}/riot/account/matches/by-puuid`,
+  MATCH_BY_ID: `${SERVER_BASE_URL}/riot/match/`,
+};
 
-
-/**
- * 
- */
 class Server {
   constructor() {
     RiotApi.setApiKey(RIOT_API_KEY);
@@ -45,76 +40,73 @@ class Server {
   }
 
   initializeRoutes() {
+    // Base url to check the server is running
     this.app.get(`${SERVER_BASE_URL}/`, this.handlePing);
+
     // Account by game name
     this.app.get(URL.ACCOUNT_BY_GAME_NAME, this.getAccountByGameName);
-    
+
     // Account by puuid
-    this.app.get(URL.ACCOUNT_BY_PUUID, this.getAccountByPuuid)
+    this.app.get(URL.ACCOUNT_BY_PUUID, this.getAccountByPuuid);
 
     // Matches by puuid
-    this.app.get(URL.MATCHES_BY_PUUID, this.getMatchesByPuuid)
+    this.app.get(URL.MATCHES_BY_PUUID, this.getMatchesByPuuid);
 
     // Match by id
-    this.app.get(URL.MATCH_BY_ID, this.getMatchById)
+    this.app.get(URL.MATCH_BY_ID, this.getMatchById);
   }
 
   async handlePing(req, res) {
-      log(`GET ${SERVER_BASE_URL}/`);
-      res.json({ message: "Server Hit" });
+    res.json({ message: "Server Hit" });
   }
 
-  async getAccountByGameName(req,res) {
-      log(`GET ${ACCOUNT_BY_GAME_NAME_URL}`);
-      const { gameName, tagLine } = req.query;
+  async getAccountByGameName(req, res) {
+    const { gameName, tagLine } = req.query;
 
-      try {
-        const url = RiotApi.getAccountByGameNameUrl(gameName, tagLine);
-        const response = await RiotApi.fetch(url);
-        return res.json(response);
-      } catch (error) {
-        log(`Error ${error}`);
-      }
+    try {
+      const url = RiotApi.getAccountByGameNameUrl(gameName, tagLine);
+      const response = await RiotApi.fetch(url);
+      return res.json(response);
+    } catch (error) {
+      log(`Error ${error}`);
+    }
   }
 
-  async getAccountByPuuid(req,res) {
-      log(`GET ${ACCOUNT_BY_PUUID_URL}`);
-      const { puuid } = req.query;
+  async getAccountByPuuid(req, res) {
+    const { puuid } = req.query;
 
-      try {
-        const url = RiotApi.getAccountByPuuidUrl(puuid);
-        const response = await RiotApi.fetch(url);
-        return res.json(response);
-      } catch (error) {
-        log(`Error ${error}`);
-      }
+    try {
+      const url = RiotApi.getAccountByPuuidUrl(puuid);
+      const response = await RiotApi.fetch(url);
+      return res.json(response);
+    } catch (error) {
+      log(`Error ${error}`);
+    }
   }
 
-  async getMatchesByPuuid(req,res) {
-      log(`GET ${MATCHES_BY_PUUID_URL}`);
-      const ids = req.query;
+  async getMatchesByPuuid(req, res) {
+    const ids = req.query;
 
-      try {
-        const url = RiotApi.getMatchesByPuuidUrl(ids);
-        const response = await RiotApi.fetch(url);
-        return res.json(response);
-      } catch (error) {
-        log(`Error ${error}`);
-      }
+    try {
+      const url = RiotApi.getMatchesByPuuidUrl(ids);
+      const response = await RiotApi.fetch(url);
+      return res.json(response);
+    } catch (error) {
+      log(`Error ${error}`);
+    }
   }
 
-  async getMatchById(req,res) {
-      log(`GET ${MATCH_BY_ID_URL}`);
-      const { matchId } = req.query;
+  async getMatchById(req, res) {
+    const { matchId } = req.query;
 
-      try {
-        const url = RiotApi.getMatchByIdUrl(matchId);
-        console.log(url);
-        const response = await RiotApi.fetch(url);
-        return res.json(response);
-      } catch (error) {
-        log(`Error ${error}`);
-      }
+    try {
+      const url = RiotApi.getMatchByIdUrl(matchId);
+      console.log(url);
+      const response = await RiotApi.fetch(url);
+      return res.json(response);
+    } catch (error) {
+      log(`Error ${error}`);
+    }
   }
 
   listen() {
